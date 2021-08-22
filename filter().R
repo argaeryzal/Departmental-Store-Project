5ca3644c60bf348467ba43496011c2fd97399117
Arga Eryzal Pradinata <argamadiun@gmail.com>
2021-08-22 21:41:36 +0700

    Add files via upload

---
+++ filter().R
@@ -0,0 +1,47 @@
+##WELCOME TO THE GUIDED PROJET!!
+
+
+
+##The 2 packages needed are dplyr and ggplot2.
+###TO INSTALL THE PACKAGES, RUN THE FOLLOWING:-
+
+install.packages("dplyr")
+install.packages("ggplot2")
+
+##WELCOME TO DATA MANIPULATION USING DPLYR
+## THE 5 FUNCTIONS OF DPLYR ARE: filter(),select(),arrange(),mutate(), and summarise()
+
+
+####1.filter()
+
+####LOAD THE DPLYR PACKAGE
+
+
+##LET'S LOAD THE DATASET
+store <-read.csv("MY DEPARTMENTAL STORE.csv")
+#VIEW THE DATASET
+
+
+
+
+###1.1.GET THE INFORMATION OF THE PRODUCT WHERE TO PRODUCT_TYPE IS 'foodgrains&spices'
+
+
+
+###1.2. GET THE INFORMATION OF THE PRODUCT WHERE IT BELONGS TO COMPANY S&B
+store1 <- filter(store, )
+View(store1)
+
+
+
+
+
+###SELF-ANALYSIS 1
+###1.3. GET THE INFORMATION OF THE PRODUCT WHERE PRODUCT_CATEGORY IS 'Dry Fruits'
+
+### This one I leave for you to think and write. 
+###ALL THE BEST!
+
+
+
+
